9a351abb32981511bd01e4cae9767a7bd16bbc16
steely-glint <thp@westhawk.co.uk>
2020-01-19 13:20:29 +0000

    Added FEC decode marker interface

---
+++ modules/phono-java-audio/src/java/com/phono/audio/codec/DecodesFEC.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.phono.audio.codec;
+
+/**
+ * Marker interface  for decoders that can (perhaps) do fec
+ * @author tim
+ */
+public interface DecodesFEC extends DecoderFace{
+    
+}
